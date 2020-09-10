54cbfe9ad5398a13160596aa30bec7270868ed32
trinh manh ha <nearha@gmail.com>
2020-09-10 17:22:23 +0700

    up bai tap 1 java

---
+++ Javahomework/MortgageCaculator/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Nhap khoan tien vay: ");
+        float loan = input.nextFloat();
+        input.nextLine();
+        System.out.printf("Nhap lai suat gan hang percent/year: ");
+        float interest = input.nextFloat();
+        double r = (double) interest / 1200;
+        input.nextLine();
+        System.out.printf("Nhap thoi gian vay (tinh bang thang): ");
+        int monthNumber = input.nextInt();
+        input.nextLine();
+        System.out.printf("So tien phai tra la: ");
+        double M = (double) loan * r * Math.pow ((1 + r), monthNumber) / (Math.pow ((1 + r), monthNumber) - 1);
+        System.out.println("So tien phai tra  sau " + monthNumber + " thang la : " +M);
+    }
+}
+
+
+
+
+
